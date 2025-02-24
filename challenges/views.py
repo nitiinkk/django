9e9a8a24721d247743b25dc8e6eb40eb603840ca
nitiinkk <nitinkumar21038@gmail.com>
2025-02-24 10:27:23 +0530

    redirects

--- challenges/views.py
+++ challenges/views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
+month_by_key = {
+    "january": "Walk for 10 mins",
+    "february": "eat for 10 mins",
+    "march": "sleep for 10 mins",
+    "april": "dance for 10 mins",
+    "may": "sing for 10 mins",
+    "june": "jump for 10 mins",
+    "july": "run for 10 mins",
+    "august": "bing watch for 10 mins",
+    "september": "shout for 10 mins",
+    "october": "yoga for 10 mins",
+    "november": "exercise for 10 mins",
+    "december": "drive for 10 mins"
+}
+
 def monthly_challenge_by_number(request, month) :
-    return HttpResponse(month)
+    monthList = list(month_by_key.keys()) #dict_keys(['a', 'b', 'c']), <class 'dict_keys'>
+    if month > len(monthList):
+        return HttpResponseNotFound("Invalid Month {}".format(month))
+    
+    current_month = monthList[month-1]
+    return HttpResponseRedirect("/challenges/" + current_month)
+
 
 def monthly_challenge(request, month) :
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Walk for 10 mins"
-    elif month == "february":
-        challenge_text = "take rest for 10 mins"
-    else:
+    try:
+        challenge_text = month_by_key[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound(" Format Not supported for month: {}".format(month))
-                                    
-    return HttpResponse(challenge_text)
